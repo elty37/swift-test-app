cbb8f692545b6b90d4e9e2ab9dab8de963054b5b
sou.mikaze <fuu.mikaze@gmail.com>
2020-09-13 10:08:56 +0900

    Initial Commit

---
+++ swift-test-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-test-app
+//
+//  Created by elty on 2020/09/13.
+//  Copyright © 2020 elty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
